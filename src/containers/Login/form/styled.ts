737fab83d0b0f8984c443fa1d22b494a97c7935a
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-03-04 22:14:23 +0100

    fix: mobile login screen

--- src/containers/Login/form/styled.ts
+++ src/containers/Login/form/styled.ts
@@ -13,6 +13,11 @@ export const LoginBox = styled(Box)`
   margin: 0 auto;
   width: 100%;
   max-width: min(70vw, 600px);
+
+  @media only screen and (max-width: ${({ theme }) =>
+      theme.breakpoints.values.md}px) {
+    max-width: min(86vw, 600px);
+  }
 `;
 
 export const InputBox = styled(motion.div)`
